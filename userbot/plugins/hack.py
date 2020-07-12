762248cc782619e86aff8a80159e72c9110fa0e8
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2020-07-12 20:00:52 +0530

    Initialize patch-1

---
+++ userbot/plugins/hack.py
@@ -0,0 +1,64 @@
+"""Emoji
+Available Commands:
+.emoji shrug
+.emoji apple
+.emoji :/
+.emoji -_-"""
+
+from telethon import events
+
+import asyncio
+from uniborg.util import admin_cmd
+from telethon.tl.functions.users import GetFullUserRequest
+
+
+
+@borg.on(admin_cmd(pattern=r"hack"))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 11)
+
+    #input_str = event.pattern_match.group(1)
+
+    #if input_str == "hack":
+
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        replied_user = await event.client(GetFullUserRequest(reply_message.from_id))
+        firstname = replied_user.user.first_name
+        usname = replied_user.user.username
+        idd = reply_message.from_id
+        if idd==636052499:
+            await event.edit("This is My Master\nI can't hack my master's Account")
+        else:
+            await event.edit("Hacking..")
+            animation_chars = [
+        
+            "`Connecting To Telegram Database....`",
+            "`Target Selected.`",
+            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
+            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
+            "`Hacking... 100%\n█████████HACKED███████████ `",
+            "`Targeted Account Hacked...\n\nPay 69$ To` @Aid_3n `To Stay Safe..`"
+            ]
+
+            for i in animation_ttl:
+
+                await asyncio.sleep(animation_interval)
+
+                await event.edit(animation_chars[i % 13])
+    else:
+        await event.edit("No User is Defined\n This Cannot Be Proceeded")
+            
