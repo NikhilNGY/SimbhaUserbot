fce7de16c4d9c9d885cf1fff9a92aad53a37f0fb
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2020-07-14 09:27:15 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+# Copyright (C) 2020 by SimbhaUserbot@Github, < https://github.com/NikhilNGY >.
 #
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# This file is part of < https://github.com/NikhilNGY/SimbhaUserbot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+# Please see < https://github.com/NikhilNGY/SimbhaUserbot/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -14,23 +14,23 @@ from userge import userge, Message, Config, versions, get_version
 LOGO_STICKER_ID, LOGO_STICKER_REF = None, None
 
 
-@userge.on_cmd("alive", about={
+@SimbhaUserbot.on_cmd("alive", about={
     'header': "This command is just for fun"}, allow_channels=False)
 async def alive(message: Message):
     await message.delete()
     await sendit(message)
     output = f"""
-**USERGE is Up and Running**
+**SimbhaUserbot is Up and Running**
 
        __Durable as a Serge__
 
-• **uptime** : `{userge.uptime}`
+• **uptime** : `{SimbhaUserbot.uptime}`
 • **python version** : `{versions.__python_version__}`
 • **pyrogram version** : `{versions.__pyro_version__}`
-• **userge version** : `{get_version()}`
+• **SimbhaUserbot version** : `{get_version()}`
 • **license** : {versions.__license__}
 • **copyright** : {versions.__copyright__}
-• **repo** : [Userge]({Config.UPSTREAM_REPO})
+• **repo** : [SimbhaUserbot]({Config.UPSTREAM_REPO})
 """
     await message.client.send_message(message.chat.id, output, disable_web_page_preview=True)
 
