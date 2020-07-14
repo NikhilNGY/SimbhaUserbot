6c599fc64f127beedb71d33c7766320fd3194939
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2020-07-14 12:29:14 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -152,7 +152,7 @@ async def carbon_api(e):
 
          file,
 
-         caption="<< Here's your carbon, \n Carbonised by [Sensible Userbot](https://www.github.com/midhunkm1294-bit/Sensible Userbot)>> ",
+         caption="<< Here's your carbon, \n Carbonised by [SimbhaUserbot](https://www.github.com/NikhilNGY/SimbhaUserbot)>> ",
 
          force_document=True,
 
@@ -160,7 +160,7 @@ async def carbon_api(e):
 
          )
 
-   os.remove('./Sensible Userbot.png')
+   os.remove('./SimbhaUserbot.jpg')
 
    driver.quit()
 
