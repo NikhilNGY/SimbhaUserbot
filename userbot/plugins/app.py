683b4bd710078eff9133789238b7ff88fb6ac85a
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2020-07-15 11:34:51 +0530

    Update app.py

--- userbot/plugins/app.py
+++ userbot/plugins/app.py
@@ -93,7 +93,7 @@ async def apkr(event):
         app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
         app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
         app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
-        app_details += "\n\n<b>Download : </b> <a href='https://t.me/joinchat/JCu-H1NikiYDgNjpjPYd4A'>Request_Here</a>"
+        app_details += "\n\n<b>Download : </b> <a href='https://t.me/NGY_BOTS'>Request_Here</a>"
         app_details += "\n\n===> @Xpl0iter <==="
         await event.edit(app_details, link_preview = True, parse_mode = 'HTML')
     except IndexError:
