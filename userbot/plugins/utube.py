9c8575c21e047f128ae4f7c1d035d5c9f7d8c7be
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2020-07-15 10:54:49 +0530

    Update utube.py

--- userbot/plugins/utube.py
+++ userbot/plugins/utube.py
@@ -1,10 +1,10 @@
 """ work with youtube """
 
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+# Copyright (C) 2020 by NikhilNGY@Github, < https://github.com/NikhilNGY >.
 #
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# This file is part of < https://github.com/NikhilNGY/SimbhaUserbot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+# Please see < https://github.com/NikhilNGY/SimbhaUserbot/blob/master/LICENSE >
 #
 # All rights reserved.
 
