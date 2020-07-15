87f9dc305aa5dc6583db5aa025e7dc26af207f03
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2020-07-15 10:44:47 +0530

    Add files via upload

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,24 @@
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+#IMG CREDITS: @NGY_BOT
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
+ALIVE_IMG = "https://telegra.ph/file/bfa4c65c3e51cfe792f62.jpg"
+ALIVE_caption = "`Simbha Userbot IS:` **ONLINE**\n\n"
+ALIVE_caption += "**Current Sat** : `Simbha Userbot Sat-2.95`\n\n"
+ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
+ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
+ALIVE_caption += "**Bot Made By @NGY_BOT \n\n"
+ALIVE_caption += "[Deploy SimbhaUserbot](GitHub.com/NikhilNGY/SimbhaUserbot)"
+#@command(outgoing=True, pattern="^.alive$")
+@borg.on(admin_cmd(pattern=r"alive"))
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.delete()
+    await borg.send_file(alive.chat_id, ALIVE_IMG,caption=ALIVE_caption)
