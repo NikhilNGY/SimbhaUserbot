66e04f46023aeaff045aef4ebb2f9a4f90261c46
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2020-07-22 19:29:52 +0530

    Update autoname.py

--- userbot/plugins/autoname.py
+++ userbot/plugins/autoname.py
@@ -20,7 +20,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d-%m-%y")
         HM = time.strftime("%H:%M")
-        name = f"{HM} 😎 {AUTONAME} 😎 {DM}"
+        name = f"N I K H I L {HSN}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
